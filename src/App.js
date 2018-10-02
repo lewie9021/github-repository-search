4bca1619d159cc31c4c07e475d286dedbe7fdaf8
Lewis Barnes <lb.barnes@live.co.uk>
2018-10-02 06:29:17 +0100

    Added header and reshuffled directory structure.

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends React.Component {
     return (
       <Router>
         <div id="app">
+          <nav class="navbar navbar-dark bg-dark" style={{marginBottom: 20}}>
+            <span class="navbar-brand mb-0 h1">Github Repository Search</span>
+          </nav>
           <Route exact path="/" component={RepositorySearchPage} />
           <Route path="/repo/:userId/:repoId" component={ReadmeViewerPage} />
         </div>
